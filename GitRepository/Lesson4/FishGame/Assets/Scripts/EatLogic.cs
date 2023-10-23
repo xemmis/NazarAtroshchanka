295b9b05e92a47a8a13df5ce697c98a4c10fd2c2
xemmis <atrogardey1@gmail.com>
2023-10-23 23:22:49 +0300

    UpgradeEatLogic

--- GitRepository/Lesson4/FishGame/Assets/Scripts/EatLogic.cs
+++ GitRepository/Lesson4/FishGame/Assets/Scripts/EatLogic.cs
@@ -7,10 +7,11 @@ using UnityEngine;
 public class EatLogic : MonoBehaviour
 {
     Transform transform;
+    [SerializeField] public Collider playerCollider;
     [SerializeField] private float _mass;
 
     private void Start()
-    {
+    {        
         transform = GetComponent<Transform>();
         _mass = GetComponent<Mass>().Value;
         transform.localScale = new Vector3(_mass, _mass, _mass);
@@ -18,19 +19,25 @@ public class EatLogic : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Enemy")
-        {
+        {           
             float enemyMass = collision.gameObject.GetComponent<Mass>().Value;
             if (_mass > enemyMass)
             {
-                transform.localScale += new Vector3(enemyMass * 0.5f, enemyMass * 0.5f, enemyMass * 0.5f);
+                Physics.IgnoreCollision(playerCollider, collision.collider, false);
+                _mass += enemyMass * 0.5f;
+                transform.localScale += new Vector3(_mass, _mass, _mass);
                 Destroy(collision.gameObject);
+            }
+            else if (_mass == enemyMass)
+            {
+                Physics.IgnoreCollision(playerCollider, collision.collider, true);
             }
             else if (_mass < enemyMass)
             {
+                Physics.IgnoreCollision(playerCollider, collision.collider, false);
                 collision.transform.localScale += new Vector3(enemyMass * 0.5f, enemyMass * 0.5f, enemyMass * 0.5f);
                 Destroy(gameObject);
             }
         }
-
     }
 }
